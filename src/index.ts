f7e05bd128e3230938acc2a3feeadba109726330
Tuguldurbayar-g <88539270+Tuguldurbayar-g@users.noreply.github.com>
2022-12-15 17:03:49 +0800

    ok

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { createYoga } from "graphql-yoga";
-import { createServer } from "node:http";
+import { createServer } from "http";
 import { schema } from "./schema";
 
 const yoga = createYoga({
